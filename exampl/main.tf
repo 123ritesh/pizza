9b2c6e4aed4213ae763a73c6916372c069b9ab3d
Ritesh Arla <ritesharla@ITAdmins-MacBook-Pro-2.local>
2018-12-18 12:38:12 -0500

    addedmain.tf

--- exampl/main.tf
+++ exampl/main.tf
@@ -1,5 +1,37 @@
+
+resource # Create a new instance of the latest Ubuntu 14.04 on an
+# t2.micro node with an AWS Tag naming it "HelloWorld"
 provider "aws" {
-region ="us-east-1"
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
 
+resource "aws_security_group_rule" "allow_all" {
+  type            = "ingress"
+  from_port       = 0
+  to_port         = 65535
+  protocol        = "tcp"
+  cidr_blocks     = ["0.0.0.0/0"]
+  prefix_list_ids = ["pl-12c4e678"]
+
+  security_group_id = "sg-123456"
+}
+output "server_public_ip"{
+    value = "${aws_instance.grunt.public_ip"
+}
